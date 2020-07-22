ffbd2202043c7fac16bc0b7ae4cbdc3dd88160c8
avtoriscenaria <fafterty@gmail.com>
2020-07-22 23:57:51 +0300

    Updates

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import PictureCard from "./PictureCard/PictureCard";
-import Modal from "./Modal/Modal";
-import Button from "./Button/Button";
-import Icon from "./Icon/Icon";
-
-export {PictureCard, Modal, Button}
--- src/constants/api.js
+++
@@ -1,6 +0,0 @@
-const api = {
-    get_pictures: {method: "GET", uri: 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'}
-};
-
-
-export {api}
