9d13dbb7a1c1811a062c8a5ac9a7bf73ec02fe97
avtoriscenaria <fafterty@gmail.com>
2020-07-22 22:31:25 +0300

    gh-pages

---
+++ build/precache-manifest.2b2756222bc5523af1f2b49291e4d8d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be8b082084131d3503394e63152c731",
+    "url": "/picture-viewer/index.html"
+  },
+  {
+    "revision": "70fa3c47090629569b72",
+    "url": "/picture-viewer/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "0b80a05015c2a641a4a6",
+    "url": "/picture-viewer/static/css/main.9fe4972d.chunk.css"
+  },
+  {
+    "revision": "70fa3c47090629569b72",
+    "url": "/picture-viewer/static/js/2.f61b529e.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/picture-viewer/static/js/2.f61b529e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b80a05015c2a641a4a6",
+    "url": "/picture-viewer/static/js/main.8bc7c148.chunk.js"
+  },
+  {
+    "revision": "273b6ead266f5222a424",
+    "url": "/picture-viewer/static/js/runtime-main.249bca0e.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.63c2c5f037886261b4ff3269ea520980.js"
+  "/picture-viewer/precache-manifest.2b2756222bc5523af1f2b49291e4d8d6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/picture-viewer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
